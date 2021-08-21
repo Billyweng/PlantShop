45b3c0ec61c1c093665e4f179e48a95f16ca4fc0
billy.weng <b7719328@gmail.com>
2021-08-21 18:47:41 +0800

    add elementui

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import "@/styles/style.scss";
+
+//引入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
   router,
